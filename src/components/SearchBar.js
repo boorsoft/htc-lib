4f527e3ebdd02eaeaa810f14da75e85b6ec4ee05
Abdrakhim Arifov <abdrroid@gmail.com>
2020-09-17 12:30:55 +0000

    SearchBar css changed, added some HTML tags

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,13 @@ export default class SearchBar extends React.Component {
       <div className="SearchBar">
         <div className="search-container">
           <input id="searchInput" className="search-input text-color-black" type="text" placeholder="Enter the book title..." />
+          <div>
+            <select name="filter" id="filter">
+              <option value="teacher">Teacher</option>
+              <option value="book">Book</option>
+              <option value="lesson">Lesson</option>
+            </select>
+          </div>
         </div>
       </div>
     )
