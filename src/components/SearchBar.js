8bdf9f4c8b07c44fbd80c0fec165cc8366f1255a
Azim Akmatov <azim.akmatov1@gmail.com>
2020-09-17 16:57:24 +0000

    clickable select icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,13 +8,11 @@ export default class SearchBar extends React.Component {
         <div className="search-container">
           <input id="searchInput" className="search-input text-color-black" type="text" placeholder="Enter the book title..." />
           <div className="select-wrapper">
-              <label>
-                <select name="filter" id="filter">
-                  <option value="teacher">Teacher</option>
-                  <option value="book">Book</option>
-                  <option value="lesson">Lesson</option>
-                </select>
-              </label>
+              <select name="filter" id="filter">
+                <option value="teacher">Teacher</option>
+                <option value="book">Book</option>
+                <option value="lesson">Lesson</option>
+              </select>
           </div>
         </div>
       </div>
