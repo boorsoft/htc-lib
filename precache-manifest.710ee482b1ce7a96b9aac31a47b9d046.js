6a54140c8a1530c88ce1693a92f3ca08c7a8469d
akmatoff <azim.akmatov1@gmail.com>
2021-04-09 06:47:42 -0400

    Updates

---
+++ precache-manifest.710ee482b1ce7a96b9aac31a47b9d046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79f47d887b1461252dab7ca3bc22efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7fc7c798599f3213ee5",
+    "url": "/static/css/main.2e8a509b.chunk.css"
+  },
+  {
+    "revision": "2b469f8f3c29962dc9e3",
+    "url": "/static/js/2.9733b898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9733b898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fc7c798599f3213ee5",
+    "url": "/static/js/main.4fb7991a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
